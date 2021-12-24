00294122f6dd2bb68feb7b098e65e9f717245b53
hdinjos <hdinjos@gmail.com>
2021-12-24 17:09:50 +0700

    delete cookie

--- databases/auth/logout.php
+++ databases/auth/logout.php
@@ -1,7 +1,12 @@
 <?php
+//delete session
 session_start();
 $_SESSION = [];
 session_unset();
 session_destroy();
+
+//delete cookie
+setcookie("uniqId", "", time() + 3600);
+setcookie("uniqName", "", time() + 3600);
 header("Location: http://localhost/learn/databases/auth/login.php");
 exit;
